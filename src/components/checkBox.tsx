eae8572b9b6183f45704cce4c0b314614f554ffc
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-31 22:31:15 +0200

    Created Check Box

---
+++ src/components/checkBox.tsx
@@ -0,0 +1,7 @@
+export default function CheckBox() {
+  return (
+    <main>
+      <h1>Check Box</h1>
+    </main>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import CheckBox from "@/components/checkBox";
+
 export default function Home() {
   return (
     <main>
       <h1>Happy Notes</h1>
+      <CheckBox />
     </main>
   );
 }
