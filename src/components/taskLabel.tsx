db476cabcbfb8d646cfc1daf9e57e8237037702b
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-01 01:54:17 +0200

    Styled task label

--- src/components/taskLabel.tsx
+++ src/components/taskLabel.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 export default function TaskLabel() {
+  const [done, setDone] = useState(false);
   return (
-    <div>
-      <h1>Task Label</h1>
+    <div onClick={() => setDone(!done)}>
+      <motion.label
+        className={`${
+          done ? "text-gray-700" : "text-black"
+        } relative ml-5 delay-200`}
+      >
+        Task Label
+        <motion.span
+          className={` ${
+            done ? "bg-gray-700" : "bg-black"
+          } absolute left-0 w-full h-0.5 translate-y-1/2 top-1/2`}
+          initial={{ width: 0 }}
+          animate={{ width: done ? "100%" : 0 }}
+          transition={{ duration: 0.1 }}
+        ></motion.span>
+      </motion.label>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import CheckBox from "@/components/checkBox";
+import TaskLabel from "@/components/taskLabel";
 
 export default function Home() {
   return (
-    <main className="bg-gray-500">
+    <main className="bg-red-500">
       <h1>Happy Notes</h1>
       <CheckBox />
+      <TaskLabel />
     </main>
   );
 }
