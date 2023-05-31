13514b2e19f7fb915bbf44eabcc0b7b48b54d558
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-01 00:45:53 +0200

    Animated CheckBox

--- src/components/checkBox.tsx
+++ src/components/checkBox.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 export default function CheckBox() {
+  const [done, setDone] = useState(false);
   return (
-    <main>
-      <h1>Check Box</h1>
-    </main>
+    <motion.button
+      onClick={() => setDone(!done)}
+      animate={{ backgroundColor: done ? "blue" : "white" }}
+      transition={{ duration: 0.1 }}
+      className="flex items-center justify-center w-8 h-8 transition-all rounded-md "
+    >
+      <svg
+        className="w-8 h-8 text-white"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth="3"
+      >
+        <motion.path
+          initial={{ pathLength: 0 }}
+          animate={{ pathLength: done ? 1 : 0 }}
+          transition={{ duration: 0.1, delay: 0.1 }}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M5 13l4 4L19 7"
+        ></motion.path>
+      </svg>
+    </motion.button>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import CheckBox from "@/components/checkBox";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-gray-500">
       <h1>Happy Notes</h1>
       <CheckBox />
     </main>
