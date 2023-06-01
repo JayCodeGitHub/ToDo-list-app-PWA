02530933985db6a71a640be665631aafc07067df
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-01 18:45:42 +0200

    Integrated dark mode to dark mode toggle

--- src/components/darkModeToggle.tsx
+++ src/components/darkModeToggle.tsx
@@ -22,10 +22,12 @@ export default function DarkModeToggle({
       style={{ WebkitTapHighlightColor: "transparent" }}
       onChange={toggleDarkMode}
     >
-      <span className="group-data-[selected]:bg-green-500 group-data-[focus-visible]:ring-2 mr-4 h-6 w-9 cursor-pointer rounded-full border-2 border-transparent bg-zinc-600 ring-offset-2 ring-offset-zinc-900 transition duration-200">
+      <span className="group-data-[selected]:bg-[#3b82f6] group-data-[focus-visible]:ring-2 mr-4 h-6 w-9 cursor-pointer rounded-full border-2 border-transparent bg-zinc-600 ring-offset-2 ring-offset-zinc-900 transition duration-200">
         <span className="group-data-[selected]:ml-3 group-data-[selected]:group-data-[pressed]:ml-2 group-data-[pressed]:w-6 block h-5 w-5 origin-right rounded-full bg-white shadow transition-all duration-200" />
       </span>
-      <span>{children}</span>
+      <span className="text-gray-900 transition-all dark:text-white">
+        {children}
+      </span>
     </AriaSwitch>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import DarkModeToggle from "@/components/darkModeToggle";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen transition-all bg-red-500 dark:bg-gray-900">
+    <main className="w-screen h-screen transition-all bg-slate-100 dark:bg-gray-900">
       <h1>Happy Notes</h1>
       <Task />
       <DarkModeToggle>Dark Mode</DarkModeToggle>
