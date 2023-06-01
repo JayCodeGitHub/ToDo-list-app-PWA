2cac063adb2f4ad84e5ca75d79fc8fbb5f06694d
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-01 18:25:39 +0200

    Integrated simple dark mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import Task from "@/components/task";
 import DarkModeToggle from "@/components/darkModeToggle";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+
+    localStorage.setItem("darkMode", !darkMode ? "dark" : "light");
+
+    document.body.classList.toggle("dark");
+  };
   return (
-    <main className="bg-red-500">
+    <main className="w-screen h-screen bg-red-500 dark:bg-gray-900">
       <h1>Happy Notes</h1>
       <Task />
-      <DarkModeToggle />
+      <DarkModeToggle isSelected={darkMode} onChange={toggleDarkMode}>
+        Dark Mode
+      </DarkModeToggle>
     </main>
   );
 }
