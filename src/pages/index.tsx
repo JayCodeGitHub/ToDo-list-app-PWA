99c0879307b5ec8704947ab1b942cc6a045d817a
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-01 16:47:47 +0200

    Styled dark mode toggle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Task from "@/components/task";
+import DarkModeToggle from "@/components/darkModeToggle";
 
 export default function Home() {
   return (
     <main className="bg-red-500">
       <h1>Happy Notes</h1>
       <Task />
+      <DarkModeToggle />
     </main>
   );
 }
