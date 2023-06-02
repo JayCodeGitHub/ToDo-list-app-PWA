6a4620badc7a779783aff8b4b62f9d8cf8ce6c44
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 17:00:09 +0200

    Styled tasks list for Mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <main className="absolute flex flex-col items-center justify-start w-full h-screen gap-5 py-16 transition-all bg-slate-100 dark:bg-gray-900">
         <Input />
-        <section className="flex flex-col items-start w-48 mt-5">
+        <section className="flex flex-col items-start w-full mt-5 md:w-60 px-14 md:px-0">
           {tasks.map((item) => (
             <Task title={item} key={item} />
           ))}
