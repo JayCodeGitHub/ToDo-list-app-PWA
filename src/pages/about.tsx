8de374d131f74b83c74d1a6660c75a3b3f9e2c17
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 17:25:36 +0200

    Styled about for Mobile

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,11 @@
 export default function About() {
   return (
     <main className="absolute flex flex-col items-center justify-start w-full h-screen gap-5 transition-all bg-slate-100 dark:bg-gray-900">
-      <section className="w-1/2 mx-auto mt-16">
+      <section className="px-4 mx-auto mt-16 md:px-0 md:w-1/2">
         <h1 className="mb-12 text-3xl text-gray-900 transition-all dark:text-white">
           About
         </h1>
-        <p className="text-xl text-gray-600 transition-all dark:text-gray-400">
+        <p className="text-gray-600 transition-all text-md md:text-xl dark:text-gray-400">
           99% of the time this is what you want to use. When hooks are stable
           and if you refactor any of your class components to use hooks, you'll
           likely move any code from componentDidMount, componentDidUpdate, and
