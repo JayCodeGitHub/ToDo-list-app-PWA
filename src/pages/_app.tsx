cd2777a844d2455dad8c4d73f320d4308d2a82ce
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 17:34:08 +0200

    Added app title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,21 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { TasksProvider } from "@/hooks/useTasks";
 import Layout from "@/components/layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <TasksProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </TasksProvider>
+    <>
+      <Head>
+        <title>ToDo App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <TasksProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </TasksProvider>
+    </>
   );
 }
