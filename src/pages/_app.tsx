fbd7abef49df311a1ee85a220c45f8e8c4b69f0a
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 16:22:15 +0200

    Created Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { TasksProvider } from "@/hooks/useTasks";
+import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <TasksProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </TasksProvider>
   );
 }
