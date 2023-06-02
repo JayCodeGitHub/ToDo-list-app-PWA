af52dc7891d2db4e63fab4b78659f3a7eefd388e
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 16:34:18 +0200

    Fix NavBar styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import DarkModeToggle from "./darkModeToggle";
 
 export default function NavBar() {
   return (
-    <nav className="absolute z-10 flex items-center justify-between w-full h-12 text-gray-900 transition-all px-28 dark:text-white">
+    <nav className="flex items-center justify-between w-full h-12 text-gray-900 transition-all px-28 dark:text-white bg-slate-100 dark:bg-gray-900">
       <ul className="flex gap-4 text-lg">
         <li>
           <Link href="/">To Do</Link>
