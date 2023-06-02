7536804bdd159e7c9531338fad9ea37fe08d4b53
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 16:35:39 +0200

    Styled About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,9 @@
 export default function About() {
   return (
-    <main>
-      <h1>About Page</h1>
+    <main className="absolute flex flex-col items-center justify-start w-full h-screen gap-5 py-16 transition-all bg-slate-100 dark:bg-gray-900">
+      <h1 className="text-gray-900 transition-all dark:text-white">
+        About Page
+      </h1>
     </main>
   );
 }
