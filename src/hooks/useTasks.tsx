1f1a2cc3414f4a119db8e07d55565e72c6332ddb
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 14:43:22 +0200

    Fix inital state of tasks

--- src/hooks/useTasks.tsx
+++ src/hooks/useTasks.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 
 interface TasksProviderProps {
   children: React.ReactNode;
@@ -12,18 +12,7 @@ interface TasksContextProps {
 const TasksContext = React.createContext({} as TasksContextProps);
 
 export const TasksProvider = ({ children }: TasksProviderProps) => {
-  const [tasks, setTasks] = useState([
-    `Buy groceries for the week`,
-    `Complete a 30-minute workout`,
-    `Call and schedule a doctor`,
-    `Reply to important emails`,
-    `Organize and declutter the living room`,
-    `Read a chapter of a book`,
-    `Pay bills and update the budget`,
-    `Start a new hobby or learn a new skill`,
-    `Write a thank-you note to a friend`,
-    `Plan a weekend getaway or day trip`,
-  ]);
+  const [tasks, setTasks] = useState<Array<string>>([]);
 
   const addTask = (task: string) => {
     setTasks((tasks) => [task, ...tasks]);
