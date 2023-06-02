1135c092c41b4bb948ed458932e1da3e9c855ecc
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 15:24:18 +0200

    Added intro animation to task

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -13,9 +13,13 @@ export default function Task({ title }: TaskProps) {
   const { removeTask } = useTasks();
   return (
     <motion.span
-      className="flex items-center justify-between w-full h-8 pl-4 my-4"
+      className="flex items-center justify-between h-8 pl-4 my-4 w-min"
       onClick={() => removeTask(title)}
       layout
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.8 }}
+      transition={{ opacity: { duration: 0.2 } }}
     >
       <motion.span className="flex items-center">
         <span onClick={() => setIsDone(!isDone)}>
