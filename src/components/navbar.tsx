aa4025407032bbb052f3ed439673a63449f018df
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 19:45:01 +0200

    Created simple mobile navigation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 import DarkModeToggle from "./darkModeToggle";
 import { useRouter } from "next/router";
@@ -5,10 +6,11 @@ import { NavigationItems } from "@/assets/navigationItems";
 import { motion } from "framer-motion";
 
 export default function NavBar() {
+  const [isHamburger, setIsHamburger] = useState(false);
   const router = useRouter();
   return (
-    <nav className="flex items-center justify-between w-full px-2 py-6 text-gray-900 transition-all md:px-28 dark:text-white bg-slate-100 dark:bg-gray-900">
-      <ul className="flex gap-4 text-lg">
+    <nav className="flex items-center justify-end w-full px-6 py-6 text-gray-900 transition-all md:justify-between md:px-28 dark:text-white bg-slate-100 dark:bg-gray-900">
+      <ul className="hidden gap-4 text-lg md:flex">
         {NavigationItems.map(({ name, href }) => (
           <li key={name}>
             <Link
@@ -34,7 +36,32 @@ export default function NavBar() {
           </li>
         ))}
       </ul>
-      <DarkModeToggle>Dark Mode</DarkModeToggle>
+      <span className="hidden md:flex">
+        <DarkModeToggle>Dark Mode</DarkModeToggle>
+      </span>
+
+      <span
+        className="flex flex-col justify-between w-6 h-5 md:hidden"
+        onClick={() => setIsHamburger(!isHamburger)}
+      >
+        <span className="w-full h-0.5 dark:bg-white bg-gray-900 rounded-sm" />
+        <span className="w-full h-0.5 dark:bg-white bg-gray-900 rounded-sm" />
+        <span className="w-full h-0.5 dark:bg-white bg-gray-900 rounded-sm" />
+      </span>
+      <span
+        onClick={() => setIsHamburger(false)}
+        className={`fixed top-0 right-0 z-10 w-4/6 h-screen translate-x-full bg-slate-900 transition-all ${
+          isHamburger ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <ul className="text-white">
+          {NavigationItems.map(({ name, href }) => (
+            <li key={name}>
+              <Link href={href}>{name}</Link>
+            </li>
+          ))}
+        </ul>
+      </span>
     </nav>
   );
 }
