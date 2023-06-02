148994ed3ae853c177693b0c40feae3f8258576f
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 17:30:23 +0200

    Fix task label dark mode

--- src/components/taskLabel.tsx
+++ src/components/taskLabel.tsx
@@ -13,7 +13,7 @@ export default function TaskLabel({ children, isDone }: TaskProps) {
           isDone
             ? "text-gray-600 dark:text-gray-400"
             : "text-gray-900 dark:text-white "
-        } relative ml-3 cursor-grab`}
+        } relative ml-3 cursor-grab transition-all`}
       >
         {children}
         <motion.span
