d139ab1d2e60fa97d165e4c86987d0f3f5304399
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-02 16:16:53 +0200

    Added simple navigation

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,18 @@
+import Link from "next/link";
+
 export default function About() {
   return (
     <main>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">To Do</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
       <h1>About Page</h1>
     </main>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,32 @@ import Task from "@/components/task";
 import DarkModeToggle from "@/components/darkModeToggle";
 import Input from "@/components/input";
 import { useTasks } from "@/hooks/useTasks";
+import Link from "next/link";
 
 export default function Home() {
   const { tasks } = useTasks();
 
   return (
-    <main className="absolute flex flex-col items-center justify-start w-screen h-screen gap-5 py-12 transition-all bg-slate-100 dark:bg-gray-900">
-      <DarkModeToggle>Dark Mode</DarkModeToggle>
-      <Input />
-      <span className="flex flex-col items-start w-48 mt-5">
-        {tasks.map((item) => (
-          <Task title={item} key={item} />
-        ))}
-      </span>
-    </main>
+    <>
+      <main className="absolute flex flex-col items-center justify-start w-screen h-screen gap-5 py-12 transition-all bg-slate-100 dark:bg-gray-900">
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">To Do</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <DarkModeToggle>Dark Mode</DarkModeToggle>
+        <Input />
+        <section className="flex flex-col items-start w-48 mt-5">
+          {tasks.map((item) => (
+            <Task title={item} key={item} />
+          ))}
+        </section>
+      </main>
+    </>
   );
 }
