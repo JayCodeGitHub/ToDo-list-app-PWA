0427cbb3cfa3b48ba09dbc23e727b7d498954abe
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-04 17:00:15 +0200

    Added new icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>ToDo App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
         <link
           rel="icon"
