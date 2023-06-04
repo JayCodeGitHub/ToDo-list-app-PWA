46283e2093cd4b5e80946a9cd4613de7141b9521
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-04 17:38:59 +0200

    Updated about Items

--- src/assets/aboutItems.ts
+++ src/assets/aboutItems.ts
@@ -1,4 +1,4 @@
 export const AboutItems = { 
     title: "About",
-    description: "To-do list app designed for productivity enthusiasts. With seamless functionality, this application allows users to effortlessly create and manage their tasks. To delete an item, simply swipe right across the screen. Built with TypeScript and Framer Motion, this app is perfect for practicing your skills while staying organized."
+    description: "To-do list app designed for productivity enthusiasts. With seamless functionality, this application allows users to effortlessly create and manage their tasks. To delete an item, simply swipe right across the screen. Built with TypeScript, Framer Motion and PWA, this app is perfect for practicing your skills while staying organized."
 }
